156ce731fdc6165b41859d701cf02117f6874a1e
vakhrushevkirill <vakhrushev.kirill93@yandex.ru>
2022-05-21 09:40:14 +0300

    Edit

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,20 +1,23 @@
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Box {
-    private AtomicBoolean openBox = new AtomicBoolean(false);
+    private volatile boolean openBox = false;
 
-    public void closeBox() throws InterruptedException {
-        for (int i = 0; i < 10; i++){
-            Thread.sleep(1000);
-            if (openBox.get() == true){
-                openBox.set(false);
-                System.out.println("Закрыл коробку");
-            }
-            if (openBox.get() == false) {
-                System.out.println("Открыл коробку");
-                openBox.set(true);
-            }
-            Thread.sleep(1000);
+    public boolean isOpenBox() {
+        return openBox;
+    }
+
+    public void setOpenBox(boolean openBox) {
+        this.openBox = openBox;
+    }
+
+    public void statusBox() throws InterruptedException {
+
+        if (openBox == true){
+            System.out.println("Закрыл коробку");
+        }
+        if (openBox == false) {
+            System.out.println("Открыл коробку");
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,35 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Box box = new Box();
+        int tackt = 10;
         Thread threadJoke = new Thread(() -> {
-            try {
-                box.closeBox();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            for (int i = 0; i < tackt; i++) {
+                try {
+                    Thread.sleep(1500);
+                    if (box.isOpenBox()) {
+                        box.setOpenBox(false);
+                        box.statusBox();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
+
         });
         threadJoke.start();
         Thread threadUser = new Thread(() -> {
-            try {
-                box.closeBox();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            for (int i = 0; i < tackt; i++){
+                try {
+                    Thread.sleep(1000);
+                    if (!box.isOpenBox()) {
+                        box.setOpenBox(true);
+                        box.statusBox();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
